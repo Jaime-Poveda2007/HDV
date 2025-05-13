ccec997c6579402187898d724cece148d76e96de
Jaime-Poveda2007 <jaime.povedam@uniagustiniana.edu.co>
2025-05-13 17:08:09 -0500

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,6 @@
+document.getElementById("SENA").onclick = function () {
+    alert("El SENA (Servicio Nacional de Aprendizaje) es una institución pública colombiana que ofrece formación gratuita a millones de ciudadanos para que desarrollen habilidades laborales y técnicas. Fue creado en 1957 y su objetivo principal es mejorar la formación profesional, promover el empleo, y fortalecer la competitividad del país. Dale click al logo para saber mas informacion");
+  };
+  document.getElementById("UNI").onclick = function () {
+    alert("La Universidad Agustiniana (conocida como Uniagustiniana) es una institución de educación superior privada ubicada en Bogotá, Colombia, inspirada en la filosofía y valores de San Agustín. Está dirigida por la Orden de San Agustín y busca formar profesionales íntegros con una sólida base ética, académica y espiritual. Dale click al logo para saber mas informacion");
+  };
